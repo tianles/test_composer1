1dddd62d3f1bccc2e3d585a13946adc7bead37c6
tianle <1401734364@qq.com>
2018-11-29 14:50:59 +0800

    首次提交

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/2/7
+ * Time: 10:51
+ */
+
+
+class Home {
+    public function index () {
+        echo "欢迎进入我的世界1.0.1";
+    }
+
+    public function pagesizes () {
+        echo "这里是我的列表页1.0.1";
+    }
+}
+?>
