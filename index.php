6da77606b57bb7d143ab5ade3a530b1277824137
tianle <1401734364@qq.com>
2018-11-29 17:14:45 +0800

    打版本补丁

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 
 class Home {
     public function index () {
-        echo "欢迎进入我的世界1.0.0";
+        echo "欢迎进入我的世界1.0.0啊";
     }
 
     public function pagesizes () {
-        echo "这里是我的列表页1.0.0";
+        echo "这里是我的列表页1.0.0啊";
     }
 }
 ?>
