1c0569862a91e46245f5cae58044e1fb532a1391
tianle <1401734364@qq.com>
2018-11-29 17:09:20 +0800

    打标签

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 
 class Home {
     public function index () {
-        echo "欢迎进入我的世界1.0.1";
+        echo "欢迎进入我的世界1.0.0";
     }
 
     public function pagesizes () {
-        echo "这里是我的列表页1.0.122222222222222";
+        echo "这里是我的列表页1.0.0";
     }
 }
 ?>
